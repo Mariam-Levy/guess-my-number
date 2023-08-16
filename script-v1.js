3b5ae5192dba34830c9b8dd44923f728597629ff
Mariam-Levy <mariamlevycode@gmail.com>
2023-08-15 22:35:34 -0300

    Refatorizando JS

--- script-v1.js
+++ script-v1.js
@@ -6,6 +6,10 @@ let highscore = 0; // highscore = puntuacion mas alto | el record comienza en 0
 //Para generar un numero aleatorio entre el 1 y el 20 y a la vez almacenarlos en una variable:
 let secretNumber = Math.trunc(Math.random() * 20) + 1; 
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -13,10 +17,10 @@ document.querySelector('.check').addEventListener('click', function() {
 
 
     if(!guess) {  //cuando el ususario no ingreso un numero
-        document.querySelector('.message').textContent = 'No Number! ❌';
+        displayMessage('No Number! ❌');
 
     } else if (guess === secretNumber) { //cuando el usuario introdujo el numero correcto.
-        document.querySelector('.message').textContent = 'Correct Number! ✅'; // arroja el mensaje que el usuario atino el numero
+        displayMessage('Correct Number! ✅'); // arroja el mensaje que el usuario atino el numero
         document.querySelector('.number').textContent = secretNumber; //muestra el numero secreto solo cuando sea el correcto.
 
         //Manipular CSS desde JS:
@@ -28,36 +32,26 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.highscore').textContent = highscore;
         }
 
-    } else if (guess > secretNumber) { // cuando el usuario ingreso un numero muy alto
+    } else if (guess !== secretNumber) { // cuando el numero es diferente al numero secreto
         if(score > 1) {
-            document.querySelector('.message').textContent = 'too high! ⬆'; // muestra el mensaje que el numero fue muy alto
-            score = score - 1;  // resta un punto del score
+            displayMessage(guess > secretNumber ? 'too high! ⬆' : 'too low! ⬇'); // cuando el numero introducido es mayor al numero secreto entonces sale el mensaje que el numero es muy alto, de lo contrario muesta el mensaje que el numero es muy bajo.
+            score = score - 1; // resta un punto del score
             document.querySelector('.score').textContent = score; // muestra el nuevo valor del score
         } else {
-            document.querySelector('.message').textContent = 'You lost the game 😫'; 
+            /* document.querySelector('.message').textContent = 'You lost the game 😫';  */
+            displayMessage('You lost the game 😫')
             document.querySelector('.score').textContent = 0; // para que el puntaje se muestre en 0, cuando se pierde el juego
             document.querySelector('body').style.backgroundColor = '#d00000'; 
         }
-        
-
-    } else if (guess < secretNumber) { // cuando el usuario ingreso un numero muy bajo
-        if(score > 1) {
-            document.querySelector('.message').textContent = 'too low! ⬇'; // muestra el mensaje que el numero fue muy bajo
-            score--;    // resta un punto del score
-            document.querySelector('.score').textContent = score; // muestra el nuevo valor del score
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game 😞';
-            document.querySelector('.score').textContent = 0; // cuando se pierde el juego, el puntaje del score seria 0.
-            document.querySelector('body').style.backgroundColor = '#d00000';
-        }
-    }
+    } 
 });
+
 
 
 document.querySelector('.again').addEventListener('click', function() {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1; 
-    document.querySelector('.message').textContent = 'Start guessing...'
+    displayMessage('Start guessing...');
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?'
     document.querySelector('.guess').value = '';
